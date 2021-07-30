a4301115ee5090ee45ab995ac062d9a9da315145
Reginald Johnson <reggie3@protonmail.com>
2021-07-30 15:39:01 -0400

    don't show the game dashboard if the user is not logged in

--- app/core/components/gameDashboard/GameDashboard.tsx
+++ app/core/components/gameDashboard/GameDashboard.tsx
@@ -1,3 +1,4 @@
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import getGames from "app/games/queries/getGames"
 import { GamesList } from "app/pages/games"
 import { usePaginatedQuery } from "blitz"
@@ -7,6 +8,11 @@ import MyGamesList from "./MyGamesList"
 interface Props {}
 
 const GameDashboard = (props: Props) => {
+  const currentUser = useCurrentUser()
+
+  if (!currentUser) {
+    return null
+  }
   return (
     <div data-testid="game-dashboard-container">
       <Suspense fallback={<div>Loading...</div>}>
